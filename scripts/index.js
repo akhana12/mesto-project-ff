0fd788e0882e8236a4d2f42e8be611bc13733448
Валентина Мужчинина <valya@Mac-mini-Valentina.local>
2024-01-15 22:32:04 +0600

    Реализована инициализация карточек при загрузке, удаление карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(item) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__title').textContent = item.name;
+    cardElement.querySelector('.card__image').alt = `Фотография локации: ${item.name}`;
+
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', () => deleteCard(cardElement));
+    cardList.append(cardElement);
+    return cardElement;
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(card) {
+    card.remove();
+};
 
 // @todo: Вывести карточки на страницу
+function initialCardList() {
+    initialCards.forEach(function (item) {
+        createCard(item)
+    });
+};
+
+
+
+initialCardList();
